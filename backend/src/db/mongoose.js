ba8daa6d095a8c8dc911e9c410491e822484d6e9
ttummala17 <91515443+ttummala17@users.noreply.github.com>
2022-04-11 16:33:14 -0500

    Update mongoose.js

--- backend/src/db/mongoose.js
+++ backend/src/db/mongoose.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
    DB Atlas mongodb+srv://expoadmin:expoadmin123@cluster0.lqrpt.mongodb.net/expo-api?retryWrites=true */
 
 mongoose.connect(process.env.MDB_CONNECT_URI,{
+   
     useNewUrlParser: true,
     useCreateIndex: true,
     userFindAndModify: false
